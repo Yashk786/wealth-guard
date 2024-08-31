0713a6e935518c0d5b90526c16776f4c483180f4
Yash Kumar <yashk7366@gmail.com>
2024-08-31 20:06:00 +0530

    changes for api

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -14,7 +14,7 @@ export default function Page() {
         </div>
         <div className="flex items-center justify-center mt-8">
           <ClerkLoaded>
-            <SignUp />
+            <SignUp path="/sign-up" />
           </ClerkLoaded>
           <ClerkLoading>
             <Loader2 className="animate-spin text-muted-foreground" />
