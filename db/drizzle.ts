e0f4c50e5b4b1d4a655017d6ba5f00cc51305fe0
Yash Kumar <87136660+Yashk786@users.noreply.github.com>
2024-09-10 01:07:12 +0530

    changes for get api

--- db/drizzle.ts
+++ db/drizzle.ts
@@ -4,4 +4,4 @@ import { drizzle } from "drizzle-orm/neon-http";
 import * as schema from "./schema";
 
 export const sql = neon(process.env.DB_URL!);
-export const dp = drizzle(sql, { schema });
+export const db = drizzle(sql, { schema });
